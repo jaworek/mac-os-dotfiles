b658fb3e3b9df54f6c9f3bc48b1a67fd0c264951
Jan Jaworski <jaworek3211@gmail.com>
2020-12-17 23:16:14 +0100

    Add restore script

---
+++ restore.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Script that installs all programs from the provided list from Homebrew 
+
+brew install something
+# check if program already exists in the list
+echo "something" | ./brew_programs.txt
+
