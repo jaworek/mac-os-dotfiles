eeee5783d1ad92fac6c0b53813f53d01377e3436
Jan Jaworski <jaworek3211@gmail.com>
2020-12-17 23:16:26 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 # Script that installs stuff from Homebrew and puts it in the file to keep track of installed packages
 # and easier backup and restore
+
+brew install something
+# check if program already exists in the list
+echo "something" | ./brew_programs.txt
