7644801dd378530558cbc2d03b9c0b91046e02d9
Jan Jaworski <jaworek3211@gmail.com>
2020-12-19 11:42:57 +0100

    Update scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Script that installs stuff from Homebrew and puts it in the file to keep track of installed packages
 # and easier backup and restore
 
+uname
+
 brew install something
 # check if program already exists in the list
 echo "something" | ./brew_programs.txt
--- unsintall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Script that uninstalls stuff from Homebrew and puts it in the file to keep track of installed packages
-# and easier backup and restore
